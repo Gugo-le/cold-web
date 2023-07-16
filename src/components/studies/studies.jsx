7c42d1641097955aea844d9e819b010f3371f714
Gugo-le <rngustmd0719@naver.com>
2023-07-16 20:47:39 +0900

    update_studiesapi

--- src/components/studies/studies.jsx
+++ src/components/studies/studies.jsx
@@ -32,7 +32,7 @@ const Profile = ({ match }) => {
                         <br />
                     </div>
                     <div className={styles.reust__title}>
-                        <h2>{nation.id}의 추천 공부 방법은?</h2>
+                        <h2>{nation.id}의 감기 예방 방법은?</h2>
                     </div>
                     <ul className={styles.result__style__wrapper}>
                         {nation.description.map((item) => {
@@ -46,42 +46,7 @@ const Profile = ({ match }) => {
                             );
                         })}
                     </ul>
-                    <div className={styles.result__advice__box}>
-                        <div className={styles.result__advice}>
-                            <a href={`${/result/}${nation.duo[0].subhead}`}>
-                                <img
-                                    src={nation.duo[0].img}
-                                    alt="mbti캐릭터"
-                                    Link="/"
-                                />
-                            </a>
-                            <div>
-                                <h4>함께하면 좋아요!</h4>
-                                <p className={styles.advice__strong}>
-                                    찰떡궁합 그 자체 {nation.duo[0].subhead}
-                                </p>
-                                <p>{nation.duo[0].des}</p>
-                            </div>
-                        </div>
-                        <div className={styles.result__advice}>
-                            <Link
-                                to={`${/result/}${nation.counter[0].subhead}`}
-                            >
-                                <img
-                                    src={nation.counter[0].img}
-                                    alt="mbti캐릭터"
-                                />
-                            </Link>
-
-                            <div>
-                                <h4>가능하면 피하는게 좋겠어요!</h4>
-                                <p className={styles.advice__strong}>
-                                    도망가세요 {nation.counter[0].subhead}
-                                </p>
-                                <p>{nation.counter[0].des}</p>
-                            </div>
-                        </div>
-                    </div>
+                    
                 
                     <div className={styles.button__box}>
                         <Link to="/" className={styles.button}>
